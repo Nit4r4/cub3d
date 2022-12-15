98f2b3d30f15dcd6632add4e3a872b0b2678d312
Verena Ferraro <vferraro@c1r5s6.42lausanne.ch>
2022-12-15 13:10:03 +0100

    i = 0

--- srcs/define_map.c
+++ srcs/define_map.c
@@ -6,7 +6,7 @@
 /*   By: vferraro <vferraro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/15 11:08:43 by vferraro          #+#    #+#             */
-/*   Updated: 2022/12/15 11:30:01 by vferraro         ###   ########.fr       */
+/*   Updated: 2022/12/15 13:09:13 by vferraro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,22 @@
 
 int	color_map(t_cub *cub)
 {
-	size_t i = 0;
+	size_t i;
 	size_t	j = 0;
 	
 	if (cub->win)
 	{
-		while (j < WIN_WID)
+		while (j < WIN_HEI)
 		{
-			while (i < WIN_HEI)
+			i = 0;
+			while (i < WIN_WID)
 			{
 				if (i % 50 == 0 || j % 50 == 0)
 					mlx_pixel_put(cub->mlx_ptr, cub->win, i, j, 0XFFFFFF);
 				else
 				{
 					if ((i > 50 && j < 450) && (j > 50 && i < 750))
-						mlx_pixel_put(cub->mlx_ptr, cub->win, i, j, 0Xcecece);
+						mlx_pixel_put(cub->mlx_ptr, cub->win, i, j, 0xcecece);
 					else
 						mlx_pixel_put(cub->mlx_ptr, cub->win, i, j, 0X000000);
 				}
@@ -39,9 +40,9 @@ int	color_map(t_cub *cub)
 		a_little_bit(cub);
 	}
 	else
-		{
-		free(cub->win);
-		critical_errors(ERR_WIN);
-		}
+	{
+	free(cub->win);
+	critical_errors(ERR_WIN);
+	}
 	return (0);
 }
